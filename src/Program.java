8913300e35e04b01a84cee0131eba9487b6c7c2a
Adam Dreyton <adam.dreyton@gmail.com>
2025-10-16 19:23:20 -0300

    Add tests for addAtPosition, removeAtPosition, indexOf, contains

--- src/Program.java
+++ src/Program.java
@@ -4,12 +4,26 @@ public class Program {
         list.addAtEnd(-1);
         list.addAtStart(21);
         list.addAtStart(60);
-        list.addAtStart(80);
-        list.addAtStart(90);
         list.addAtEnd(1);
         list.addAtEnd(10);
-        list.addAtEnd(15);
+        list.addAtPosition(100, 3);
+        list.addAtPosition(200, 1);
+        list.addAtPosition(300, 0);
+        list.addAtPosition(400, 8);
 
+        System.out.println(list);
+
+        System.out.println("Index of 21: " + list.indexOf(21));
+
+        System.out.println("Contains 100? " + list.contains(100));
+        System.out.println("Contains 101? " + list.contains(101));
+
+        list.removeAtPosition(0);
+        System.out.println(list);
+        list.removeAtPosition(7);
+        System.out.println(list);
+
+        list.removeAtPosition(2);
         System.out.println(list);
     }
 }
